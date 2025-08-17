37c3058ada14a889be29402726020eb092de9810
hoseinian <a.hoseinian13@gmail.com>
2025-08-17 05:34:14 +0330

    address in product

--- src/lib/getProducts.ts
+++ src/lib/getProducts.ts
@@ -2,12 +2,9 @@ import { Product } from "@/types/product";
 
 export async function getProducts(category: string): Promise<Product[]> {
   try {
-    const baseUrl =
-      process.env.NEXT_PUBLIC_SITE_URL ||
-      (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3008");
-
-    const res = await fetch(`${baseUrl}/api/products?category=${category}`, {
-      cache: "no-store",
+    
+   const res = await fetch(`/api/products?category=${category}`, {
+  cache: "no-store",
     });
 
     if (!res.ok) {
