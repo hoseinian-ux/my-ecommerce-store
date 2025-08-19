63a3a306dce597b24b0eb9811cc13af9c73dc9d9
hoseinian <a.hoseinian13@gmail.com>
2025-08-19 07:57:27 +0330

    telegram edit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -17,16 +17,13 @@ export default function ContactPage() {
           <p>
             <Phone /> 09378623292
           </p>
-        </div>
-
-        <div className={styles.socialLinks}>
-          <div className={styles.telegram}>
-            <svg
+          <svg 
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
+              width="20px"
             >
               <path
                 strokeLinecap="round"
@@ -34,17 +31,18 @@ export default function ContactPage() {
                 d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
               />
             </svg>
-            <span>@A_hosseinian2</span>
-          </div>
-
-          <a
+            <span  className={styles.telegram}>@A_hosseinian2</span>
+            <Github />
+             <a
             href="https://github.com/hoseinian-ux"
             target="_blank"
             className={styles.github}
           >
-            <Github />
+             <span  className={styles.telegram}>hoseinian-ux</span>
           </a>
         </div>
+
+        
       </div>
       <div className={styles.messageBox}>
        <p>
